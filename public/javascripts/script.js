dcdfbde32ce10c8b1be3c19956e7f7db679a639d
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-08-31 16:18:19 -0600

    Working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,16 @@
 
 
-var delete_button = document.getElementById("xbutton");
-var response_box = document.getElementById("response_box");
-var parent = document.getElementById("parent");
+var delete_button = document.getElementsByClassName("xbutton");
 
-delete_button.addEventListener("click", function(){
+for (var i=0; i<delete_button.length; i++) {
+	var response_box = document.getElementsByClassName("response_box");
+	var parent = document.getElementById("parent");
+delete_button[i].addEventListener("click", function(){
 	console.log("is this working?");
-	parent.removeChild(response_box);
+	parent.removeChild(this.parentElement);
+	numOfEmails = numOfEmails - 1;
 });
+};
 
 
 
